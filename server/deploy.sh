dd19be111cc42e692fcdaabbe535f6d8a3c5cae1
Ofer Sadan <ofersadan85@gmail.com>
2022-04-07 21:46:16 +0300

    new deploy script to match with stack deploy

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-DOCKER_COMPOSE_YML="${HOME}/docker-compose.yml"
-docker compose -f ${DOCKER_COMPOSE_YML} pull
-docker compose -f ${DOCKER_COMPOSE_YML} up -d --remove-orphans
+DOCKER_COMPOSE="${HOME}/docker-compose.yml"
+DOCKER_COMPOSE_STACK="${HOME}/docker-compose-stack.yml"
+
+docker compose -f "${DOCKER_COMPOSE}" pull
+docker compose -f "${DOCKER_COMPOSE}" up -d --remove-orphans
+
+deploy_stack () {
+    ENV_FILE="${HOME}/${1}.env"
+    if [ -f ${ENV_FILE} ]; then
+        docker-compose -f "${DOCKER_COMPOSE_STACK}" --env-file "${ENV_FILE}" pull
+        docker-compose -f "${DOCKER_COMPOSE_STACK}" --env-file "${ENV_FILE}" --project-name "${1}" up -d --remove-orphans
+    fi
+}
+
+deploy_stack development
+deploy_stack production
